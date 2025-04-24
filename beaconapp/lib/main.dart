09a58afbb540aa74026c854f8e43da433b086f7c
Eric <eric35linares@gmail.com>
2025-04-24 00:57:41 -0700

    Adding the first draft of the calendar. Trying to figure out main()

--- beaconapp/lib/main.dart
+++ beaconapp/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:beaconapp/authentication_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-void main() => runApp(const MainApp());
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -10,7 +16,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
       home: const AuthenticationScreen(),
@@ -18,3 +24,40 @@ class MainApp extends StatelessWidget {
   }
 }
 
+class MyApp extends StatefulWidget {
+  const MyApp( {super.key} );
+
+  @override
+  State<MyApp> createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  DateTime today = DateTime.now();
+
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(title: Text("Welcome To Canada College!")),
+    body: content(),
+  );
+}
+
+  Widget content() {
+    return Column(
+      children: [
+        Text("Canada College Schedule"),
+        Container(
+          child: TableCalendar(
+            locale: "en_US",
+            headerStyle: 
+              HeaderStyle(formatButtonVisible: false, titleCentered: true),
+            availableGestures: AvailableGestures.all,
+            focusedDay: today, 
+            firstDay: DateTime.utc(2025, 01, 01), 
+            lastDay: DateTime.utc(2030, 06, 3)
+            ),
+        ),
+      ],
+    );
+  }
+}
